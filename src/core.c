a11cdc8d1be4fa7ef5b43547ac9c05781e1a5fe6
Daniel Wieczorek <daniel-wieczorek@web.de>
2014-06-15 11:40:47 +0200

    added padding for password

--- src/core.c
+++ src/core.c
@@ -2,11 +2,13 @@
 #include "core.h"
 
 void writeEncryptedRecord(char* username, char* site, char* password, char* key, char* fileName){
-              char* paddedusername = calloc(sizeof(char), 256);
-            sprintf( paddedusername, "%0256d", username );
+    char* paddedusername = calloc(sizeof(char), 256);
+    sprintf( paddedusername, "%0256d", username );
+    char* paddedpassword = calloc(sizeof(char), 256);
+    sprintf( paddedpassword, "%0256d", username );
     char * encrypted_username = encryptString(username, 256, key, TWOFISH);
     char * hashed_site = hashString(site, SHA512);
-    char * encrypted_password = encryptString(password, getStringLength(password), key, TWOFISH);
+    char * encrypted_password = encryptString(paddedpassword, 256, key, TWOFISH);
     
     Record* record = createRecord(encrypted_username, hashed_site, encrypted_password);
     saveRecord(fileName, record);
