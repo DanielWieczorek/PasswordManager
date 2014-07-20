9746e9f7c9501b2da112434a3813bdae13422176
Daniel Wieczorek <daniel-wieczorek@web.de>
2014-07-20 15:02:16 +0200

    the password manager is now usable

--- main.c
+++ main.c
@@ -9,8 +9,62 @@
 #define NO_INPUT 1
 #define TOO_LONG 2
 static int getLine(char *prmpt, char *buff, size_t sz);
+void insertEntry();
+void retrieveEntry();
+
+char *keys[2]={"i","r"};
+void (*function[2])()={insertEntry,retrieveEntry};
 
 int main(int argc, char** argv) {
+    char *line = calloc(sizeof(char),20);
+    while(strcmp(line,"exit")){
+        getLine("What would you like to do?",line, 20);
+        int i=0, foundIndex=0;
+        for(i=0; i<sizeof(keys);i++)
+            if(!strcmp(line,keys[i])){
+                foundIndex = i;
+                break;
+            }
+        (*function[foundIndex])();
+    }
+    return 0;
+}
+
+void retrieveEntry(){
+    initCryptographyModule();
+    char* fileName = "./pw";
+    char* site = calloc(sizeof (char), 20);
+    char* key = calloc(sizeof (char), 50);
+    getLine("please enter the site name:\n", site, 20);
+    getLine("please enter the key:\n", key, 50);
+    char* hashedKey = hashString(key, SHA256);
+
+    if (!doesCredentialEntryExist(fileName)) {
+        printf("there are no credentials set. Credentials with the key entered are created");
+        writeEncryptedRecord("foo", CREDENTIAL_SITE, key, hashedKey, fileName);
+    }
+    if (checkCredentials(hashedKey, fileName)) {
+        printf("the hashed key is: %s\n", hashedKey);
+        free(key);
+        Record* encrypted = retrieveEncryptedRecord(site, fileName);
+        if (encrypted) {
+            Record *decrypted = decryptRecord(encrypted, hashedKey);
+            printf("encrypted username: %s\n", decrypted->username);
+            printf("encrypted site: %s\n", decrypted->site);
+            printf("encrypted password: %s\n", decrypted->password);
+            free(decrypted);
+        } else
+            printf("nothing found\n");
+        free(encrypted);
+    } else
+        printf("the credentials are wrong. sorry!");
+    free(site);
+    free(hashedKey);
+    freeCryptographyModuleMemory();
+}
+
+
+void insertEntry(){
     initCryptographyModule();
     char* fileName = "./pw";
     char* username = calloc(sizeof (char), 20);
@@ -21,23 +75,16 @@ int main(int argc, char** argv) {
     getLine("please enter the key:\n", key, 50);
     char* hashedKey = hashString(key, SHA256);
 
-    if (!doesCredentialEntryExist(fileName)){
+    if (!doesCredentialEntryExist(fileName)) {
         printf("there are no credentials set. Credentials with the key entered are created");
         writeEncryptedRecord("foo", CREDENTIAL_SITE, key, hashedKey, fileName);
     }
     if (checkCredentials(hashedKey, fileName)) {
-
-
-
         printf("the hashed key is: %s\n", hashedKey);
-
         free(key);
         char* password = generatePassword(12, BASE85);
         printf("the generated password is: %s\n", password);
-
         writeEncryptedRecord(username, site, password, hashedKey, fileName);
-
-
         Record* encrypted = retrieveEncryptedRecord(site, fileName);
         if (encrypted) {
             Record *decrypted = decryptRecord(encrypted, hashedKey);
@@ -48,8 +95,8 @@ int main(int argc, char** argv) {
             free(decrypted);
         } else
             printf("nothing found\n");
-    free(encrypted);
-    free(password);
+        free(encrypted);
+        free(password);
     } else
         printf("the credentials are wrong. sorry!");
     free(username);
@@ -57,7 +104,7 @@ int main(int argc, char** argv) {
     free(hashedKey);
 
     freeCryptographyModuleMemory();
-    return 0;
+
 }
 
 static int getLine(char *prmpt, char *buff, size_t sz) {
