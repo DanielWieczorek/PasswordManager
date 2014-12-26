fde69d5154527e68937da22522e24da55beb05c6
Daniel Wieczorek <DWI@Daniels-Mac-Pro.fritz.box>
2014-12-26 13:13:15 +0100

    added array lib header

---
+++ dwiarray.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   dwiarray.h
+ * Author: DWI
+ *
+ * Created on December 16, 2014, 3:09 PM
+ */
+
+#ifndef DWIARRAY_H
+#define	DWIARRAY_H
+
+#define GET_NUMBER_OF_ELEMENTS(x) (sizeof (x) / sizeof (x[0]))
+
+
+#endif	/* DWIARRAY_H */
+
